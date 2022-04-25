e4db8cacfc1cdbf109c5a404cb624f48b9a0c1a7
4SH1N <20546366+haustasis@users.noreply.github.com>
2022-04-25 15:19:39 +0200

    Stage 3/5: Guess the age

--- src/bot/SimpleBot.java
+++ src/bot/SimpleBot.java
@@ -10,10 +10,16 @@ public class SimpleBot {
         System.out.println("I was created in 2018.");
         System.out.println("Please, remind me your name.");
 
-        // reading a name
-        Scanner sc = new Scanner(System.in);
-        String name = sc.nextLine();
+        String name = scanner.nextLine();
 
-        System.out.printf("What a great name you have, %s!", name);
+        System.out.println("What a great name you have, " + name + "!");
+        System.out.println("Let me guess your age.");
+        System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
+
+        // reading all remainders
+        int[] arrA = {scanner.nextInt(), scanner.nextInt(), scanner.nextInt()};
+        int age = (arrA[0]*70 + arrA[1]*21 + arrA[2]*15) % 15;
+
+        System.out.printf("Your age is %d; that's a good time to start programming!", age);
     }
 }
