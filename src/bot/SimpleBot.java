e7c7dfc957ee3a53b57b9b9feacc52d84bab06cc
4SH1N <20546366+haustasis@users.noreply.github.com>
2022-04-25 14:19:35 +0200

    alt 3 first commit

--- src/bot/SimpleBot.java
+++ src/bot/SimpleBot.java
@@ -3,10 +3,12 @@ package bot;
 public class SimpleBot {
 
     public static void main(String[] args) {
-        botGreeting("Paca", 2022);
+        System.out.printf("Hello!, my name is %s.\nI was created in %s", BOT_NAME, BIRTH_YEAR);
     }
 
-    private static void botGreeting(String name, int birthYear) {
-        System.out.printf("Hello! my name is %s. %nI was created in %d.", name, birthYear);
-    }
+    private static final String BOT_NAME = "Sheddy";
+    private static final String BIRTH_YEAR = "2020";
+
+
+
 }
