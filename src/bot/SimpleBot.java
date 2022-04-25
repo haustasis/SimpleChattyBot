22ed0cd4bfd827be05e79f11df3f16ebaf81d81b
4SH1N <20546366+haustasis@users.noreply.github.com>
2022-04-25 13:05:49 +0200

    Stage 1/5: Chatty Bot welcomes you

---
+++ src/bot/SimpleBot.java
@@ -0,0 +1,9 @@
+package bot;
+
+public class SimpleBot {
+
+    public static void main(String[] args) {
+        System.out.println("Hello! My name is Dopamine.");
+        System.out.println("I was created in 1981.");
+    }
+}
